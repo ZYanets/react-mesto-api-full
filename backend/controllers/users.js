22aad2d3700806f9e8febfadf7badd2b44dcae3d
ZYanets <ZYanets@yandex.ru>
2022-02-15 16:40:21 +0900

    fix: авторизация

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -46,8 +46,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: false,
           secure: true,
-        })
-        .send(token);
+        });
     })
     .catch(next);
 };
